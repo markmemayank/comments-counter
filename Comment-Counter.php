904dbeeb6332a8b0d0235dc4f9f7f29c57102daa
Mayank Kumar <96015005+markmemayank@users.noreply.github.com>
2023-08-12 19:04:15 +0530

    Update Comment-Counter.php

--- Comment-Counter.php
+++ Comment-Counter.php
@@ -11,29 +11,28 @@ Author URI: https://markmemayank.com/
 function comment_counter_display() {
     $comment_counts = wp_count_comments(); // Get comment counts
 
-    $total_comments = esc_html($comment_counts->total_comments);
-    $approved_comments = esc_html($comment_counts->approved);
-    $pending_comments = esc_html($comment_counts->moderated);
-    $spam_comments = esc_html($comment_counts->spam);
-    $trash_comments = esc_html($comment_counts->trash);
+    $total_comments = $comment_counts->total_comments;
+    $approved_comments = $comment_counts->approved;
+    $pending_comments = $comment_counts->moderated;
+    $spam_comments = $comment_counts->spam;
+    $trash_comments = $comment_counts->trash;
 
     echo '<div class="comment-counter">';
-    echo '<h3>Comment Counts</h3>';
+    echo '<h3>' . esc_html__('Comment Counts') . '</h3>';
     echo '<ul>';
-    echo '<li>All Comments: ' . $total_comments . '</li>';
-    echo '<li>Approved Comments: ' . $approved_comments . '</li>';
-    echo '<li>Pending Comments: ' . $pending_comments . '</li>';
-    echo '<li>Spam Comments: ' . $spam_comments . '</li>';
-    echo '<li>Trash Comments: ' . $trash_comments . '</li>';
+    echo '<li>' . esc_html__('All Comments:') . ' ' . esc_html($total_comments) . '</li>';
+    echo '<li>' . esc_html__('Approved Comments:') . ' ' . esc_html($approved_comments) . '</li>';
+    echo '<li>' . esc_html__('Pending Comments:') . ' ' . esc_html($pending_comments) . '</li>';
+    echo '<li>' . esc_html__('Spam Comments:') . ' ' . esc_html($spam_comments) . '</li>';
+    echo '<li>' . esc_html__('Trash Comments:') . ' ' . esc_html($trash_comments) . '</li>';
     echo '</ul>';
     echo '</div>';
 }
 
-
 function comment_counter_widget() {
     wp_add_dashboard_widget(
         'comment_counter_widget',
-        'Comment Counter',
+        esc_html__('Comment Counter'),
         'comment_counter_display'
     );
 }

d43c4d6f23301e3217a79453f3fd7e032f78eef2
Mayank Kumar <96015005+markmemayank@users.noreply.github.com>
2024-01-17 19:46:44 +0530

    Update Comment-Counter.php

--- Comment-Counter.php
+++ Comment-Counter.php
@@ -1,6 +1,6 @@
 <?php
 // Exit if accessed directly.
-if ( ! defined( 'ABSPATH' ) ) exit;
+if (!defined('ABSPATH')) exit;
 
 /*
 Plugin Name: Comments Counter
@@ -11,7 +11,7 @@ Author: Mayank Kumar
 Author URI: https://markmemayank.com/
 */
 
-function cc_comment_counter_display() {
+function cc_comments_counter_display() {
     $comment_counts = wp_count_comments(); // Get comment counts
 
     $total_comments = $comment_counts->total_comments;
@@ -21,24 +21,24 @@ function cc_comment_counter_display() {
     $trash_comments = $comment_counts->trash;
 
     echo '<div class="comment-counter">';
-    echo '<h3>' . esc_html__('Comment Counts') . '</h3>';
+    echo '<h3>' . esc_html__('Comment Counts', 'comments-counter') . '</h3>';
     echo '<ul>';
-    echo '<li>' . esc_html__('All Comments:') . ' ' . esc_html($total_comments) . '</li>';
-    echo '<li>' . esc_html__('Approved Comments:') . ' ' . esc_html($approved_comments) . '</li>';
-    echo '<li>' . esc_html__('Pending Comments:') . ' ' . esc_html($pending_comments) . '</li>';
-    echo '<li>' . esc_html__('Spam Comments:') . ' ' . esc_html($spam_comments) . '</li>';
-    echo '<li>' . esc_html__('Trash Comments:') . ' ' . esc_html($trash_comments) . '</li>';
+    echo '<li>' . sprintf(esc_html__('All Comments: %s', 'comments-counter'), esc_html($total_comments)) . '</li>';
+    echo '<li>' . sprintf(esc_html__('Approved Comments: %s', 'comments-counter'), esc_html($approved_comments)) . '</li>';
+    echo '<li>' . sprintf(esc_html__('Pending Comments: %s', 'comments-counter'), esc_html($pending_comments)) . '</li>';
+    echo '<li>' . sprintf(esc_html__('Spam Comments: %s', 'comments-counter'), esc_html($spam_comments)) . '</li>';
+    echo '<li>' . sprintf(esc_html__('Trash Comments: %s', 'comments-counter'), esc_html($trash_comments)) . '</li>';
     echo '</ul>';
     echo '</div>';
 }
 
-function cc_comment_counter_widget() {
+function cc_comments_counter_widget() {
     wp_add_dashboard_widget(
-        'cc_comment_counter_widget',
-        esc_html__('Comment Counter'),
-        'cc_comment_counter_display'
+        'cc_comments_counter_widget',
+        esc_html__('Comment Counter', 'comments-counter'),
+        'cc_comments_counter_display'
     );
 }
 
-add_action('wp_dashboard_setup', 'cc_comment_counter_widget');
+add_action('wp_dashboard_setup', 'cc_comments_counter_widget');
 ?>
